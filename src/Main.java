a4138b8e91770ae2586695c3328e059c2efc273a
Christoffer Morales <christofferm91@hotmail.com>
2024-11-05 11:10:55 +0100

    flyttat det som va i main till View-->Scene

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,15 @@
-
-import View.GameMap;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-    private static final double WINDOWSIZE = 450;
-    private Label statusLabel;
-
-    @Override
-    public void start(Stage primaryStage) {
-        //windows ruta
-        AnchorPane anchorPane = new AnchorPane();
-        anchorPane.setPrefSize(WINDOWSIZE, WINDOWSIZE);
-
-        // spelbräda för server o klient
-        GameMap serverMap = new GameMap(true);
-        GameMap clientMap = new GameMap(false);
-
-        // positionering spelplan för serverplan och client
-        AnchorPane playerPane = serverMap.getGameMapPane();
-        playerPane.setLayoutX(50);
-        playerPane.setLayoutY(50);
-
-        AnchorPane opponentPane = clientMap.getGameMapPane();
-        opponentPane.setLayoutX(GameMap.GRID_SIZE * GameMap.CELL_SIZE + 150);
-        opponentPane.setLayoutY(50);
-
-        // underrubrik för spelplanerna
-        Label serverLabel = new Label("Serverns spelplan");
-        serverLabel.setLayoutX(150);
-        serverLabel.setLayoutY(10);
-        serverLabel.setTextFill(Color.RED);
-
-        Label clientLabel = new Label("Klientens spelplan");
-        clientLabel.setLayoutX(GameMap.GRID_SIZE * GameMap.CELL_SIZE + 250);
-        clientLabel.setLayoutY(10);
-        clientLabel.setTextFill(Color.GREEN);
-
-        statusLabel = new Label("Placera dina skepp!");
-        statusLabel.setLayoutX(30);
-        statusLabel.setLayoutY(GameMap.GRID_SIZE * GameMap.CELL_SIZE + 110);
-
-        anchorPane.getChildren().addAll(playerPane, opponentPane, serverLabel, clientLabel, statusLabel);
-
-        Scene scene = new Scene(anchorPane);
-        primaryStage.setTitle("Sänka Skepp");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
--- src/View/Scene.java
+++ src/View/Scene.java
@@ -1,6 +1,58 @@
 package View;
 
-public class Scene {
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-    // javaFX här, main scene
+public class Scene extends Application {
+    private static final double WINDOWSIZE = 450;
+    private Label statusLabel;
+
+    @Override
+    public void start(Stage primaryStage) {
+        //windows ruta
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setPrefSize(WINDOWSIZE, WINDOWSIZE);
+
+        // spelbräda för server o klient
+        GameMap serverMap = new GameMap(true);
+        GameMap clientMap = new GameMap(false);
+
+        // positionering spelplan för serverplan och client
+        AnchorPane playerPane = serverMap.getGameMapPane();
+        playerPane.setLayoutX(50);
+        playerPane.setLayoutY(50);
+
+        AnchorPane opponentPane = clientMap.getGameMapPane();
+        opponentPane.setLayoutX(GameMap.GRID_SIZE * GameMap.CELL_SIZE + 150);
+        opponentPane.setLayoutY(50);
+
+        // underrubrik för spelplanerna
+        Label serverLabel = new Label("Serverns spelplan");
+        serverLabel.setLayoutX(150);
+        serverLabel.setLayoutY(10);
+        serverLabel.setTextFill(Color.RED);
+
+        Label clientLabel = new Label("Klientens spelplan");
+        clientLabel.setLayoutX(GameMap.GRID_SIZE * GameMap.CELL_SIZE + 250);
+        clientLabel.setLayoutY(10);
+        clientLabel.setTextFill(Color.GREEN);
+
+        statusLabel = new Label("Placera dina skepp!");
+        statusLabel.setLayoutX(30);
+        statusLabel.setLayoutY(GameMap.GRID_SIZE * GameMap.CELL_SIZE + 110);
+
+        anchorPane.getChildren().addAll(playerPane, opponentPane, serverLabel, clientLabel, statusLabel);
+
+        javafx.scene.Scene scene = new javafx.scene.Scene(anchorPane);
+        primaryStage.setTitle("Sänka Skepp");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
